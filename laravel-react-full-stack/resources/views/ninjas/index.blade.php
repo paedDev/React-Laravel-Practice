ded0a72a5c92a5bac272ec70df6496f636f3b659
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-04-30 20:22:51 +0800

    learned how to use the blade directives (if statement and foreach)

--- laravel-react-full-stack/resources/views/ninjas/index.blade.php
+++ laravel-react-full-stack/resources/views/ninjas/index.blade.php
@@ -7,20 +7,22 @@
 </head>
 <body>
     <h2>Currently Available Ninjas</h2>
+    {{-- blade directives conditional statement --}}
+    @if ($greeting == "hello")
+    <p>Hi from insde the if statement</p>
+    @endif
     <p>{{$greeting}}</p>
+
+    {{-- foreach  --}}
     <ul>
-        <li>Naruto</li>
-        <li>Sasuke</li>
-        <li>Kakashi</li>
-        <li>
-            <a href="/ninjas/{{$ninjas[0]["id"]}}">{{$ninjas[0]['name']}}
-            </a>
-        </li>
-        <li>
-            <a href="/ninjas/{{$ninjas[1]["id"]}}">
-                {{$ninjas[1]['name']}}
-                </a>
+        @foreach($ninjas as $ninja)
+            <li>
+                <p>
+                    {{$ninja['name']}}
+                </p>
+                <a href="/ninjas/{{$ninja['id']}}">View Details</a>
             </li>
+        @endforeach
     </ul>
 </body>
 </html>
