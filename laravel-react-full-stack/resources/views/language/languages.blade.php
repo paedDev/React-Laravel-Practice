383a416a1b3d0ee9b6c11c24173f516d171bcacb
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-05-03 13:50:29 +0800

    changed the design in the language-card

--- laravel-react-full-stack/resources/views/language/languages.blade.php
+++ laravel-react-full-stack/resources/views/language/languages.blade.php
@@ -5,13 +5,13 @@
     <div class="w-5/6 mx-auto ">
         <ul class="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5 lg:p-4 p-0 font-bold text-sky-500 space-x-4 ">
             @foreach ($languages as $language)
-            <li class="space-x-4  hover:scale-105 duration-500 transform">
+            <li class="space-x-4  hover:scale-105 duration-500 transform hover:bg-sky-200 hover:text-white rounded-lg group">
                   <a href="/languages/{{$language['id']}}">
-                <x-language-card class="p-4 w-52 h-32 overflow-auto flex justify-center items-center flex-col lg:space-y-5 space-y-0">
+                <x-language-card class="p-4 w-full h-32 overflow-auto flex justify-center items-center flex-col lg:space-y-5 space-y-0">
                     <h1>
                         {{$language['title']}}
                     </h1>
-                    <button class="inline-block bg-gradient-to-r from-sky-300 to-purple-800 p-1 bg-clip-text text-transparent cursor-pointer text-xs">Click for more details</button>
+                    <button class="inline-block bg-gradient-to-r from-sky-300 to-purple-800 p-1 bg-clip-text text-transparent cursor-pointer text-xs opacity-0 group-hover:opacity-100 transition-opacity-100 duration-500">Click for more details</button>
                  
                     
                 </x-language-card>
